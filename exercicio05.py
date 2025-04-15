5d47600a5d977f45e251d8dc85b4707015f9289b
Julia Beatriz <julia.beatriz206487@gmai.com>
2025-04-15 10:31:48 -0300

    exercicio05 feito

---
+++ exercicio05.py
@@ -0,0 +1,8 @@
+valor1 = int(input("digite o primeiro valor: "))
+valor2 = int(input("digite o segundo valor: "))
+
+while valor2 == 0:
+    valor2 = int(input("digite o segundo valor novamente: "))
+divisao = valor1 / valor2
+print(divisao)
+
